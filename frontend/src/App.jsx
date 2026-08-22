8ee65208403aa4ce2871597641ef7debcbc80ee9
Nishu Kumar <nishuk342@gmail.com>
2026-08-22 15:41:23 +0530

    Test CI pipeline

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { router } from "./app.routes.jsx"
 import { AuthProvider } from "./features/auth/auth.context.jsx"
 import { InterviewProvider } from "./features/interview/interview.context.jsx"
 
-function App( {
+function App() {
 
   return (
     <AuthProvider>
